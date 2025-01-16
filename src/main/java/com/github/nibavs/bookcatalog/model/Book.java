6b254ad909b14b7380c51b27dddccb26aec11457
Nikita Bavs <nbavsunovsky@gmail.com>
2025-01-16 14:37:50 +0700

    Getters and setters added

---
+++ src/main/java/com/github/nibavs/bookcatalog/model/Book.java
@@ -0,0 +1,68 @@
+package com.github.nibavs.bookcatalog.model;
+
+public class Book {
+    private int id;
+    private String title;
+    private String author;
+    private int year;
+    private int pages;
+    private Status status;
+
+    public Book(int id, String title, String author, int year, int pages, Status status) {
+        this.id = id;
+        this.title = title;
+        this.author = author;
+        this.year = year;
+        this.pages = pages;
+        this.status = status;
+    }
+
+    // МБ удалить, пока что для теста сеттер
+    public void setId(int id) {
+        this.id = id;
+    }
+
+    public int getId() {
+        return id;
+    }
+
+    public String getTitle() {
+        return title;
+    }
+
+    public void setTitle(String title) {
+        this.title = title;
+    }
+
+    public String getAuthor() {
+        return author;
+    }
+
+    public void setAuthor(String author) {
+        this.author = author;
+    }
+
+    public int getYear() {
+        return year;
+    }
+
+    public void setYear(int year) {
+        this.year = year;
+    }
+
+    public int getPages() {
+        return pages;
+    }
+
+    public void setPages(int pages) {
+        this.pages = pages;
+    }
+
+    public Status getStatus() {
+        return status;
+    }
+
+    public void setStatus(Status status) {
+        this.status = status;
+    }
+}
