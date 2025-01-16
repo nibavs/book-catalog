fbb543f9197ae2f6b777f469542f2a280750b486
Nikita Bavs <nbavsunovsky@gmail.com>
2025-01-16 14:40:06 +0700

    Refactor

--- src/main/java/com/github/nibavs/bookcatalog/MainApp.java
+++ src/main/java/com/github/nibavs/bookcatalog/MainApp.java
@@ -7,12 +7,12 @@ import javafx.stage.Stage;
 
 import java.io.IOException;
 
-public class HelloApplication extends Application {
+public class MainApp extends Application {
     @Override
     public void start(Stage stage) throws IOException {
-        FXMLLoader fxmlLoader = new FXMLLoader(HelloApplication.class.getResource("hello-view.fxml"));
-        Scene scene = new Scene(fxmlLoader.load(), 320, 240);
-        stage.setTitle("Hello!");
+        FXMLLoader fxmlLoader = new FXMLLoader(MainApp.class.getResource("MainView.fxml"));
+        Scene scene = new Scene(fxmlLoader.load(), 800, 600);
+        stage.setTitle("Book Catalog");
         stage.setScene(scene);
         stage.show();
     }
--- src/main/java/module-info.java
+++ src/main/java/module-info.java
@@ -1,8 +1,15 @@
 module com.github.nibavs.bookcatalog {
     requires javafx.controls;
     requires javafx.fxml;
+    requires java.sql;
+    requires org.postgresql.jdbc;
+    requires java.desktop;
 
 
     opens com.github.nibavs.bookcatalog to javafx.fxml;
     exports com.github.nibavs.bookcatalog;
+    exports com.github.nibavs.bookcatalog.model;
+    opens com.github.nibavs.bookcatalog.model to javafx.fxml;
+    exports com.github.nibavs.bookcatalog.controller;
+    opens com.github.nibavs.bookcatalog.controller to javafx.fxml;
 }
