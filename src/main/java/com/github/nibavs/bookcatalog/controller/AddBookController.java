b4ecef6db18b6eb17279c29c3b94505a0f7aa41d
Nikita Bavs <nbavsunovsky@gmail.com>
2025-01-18 12:13:42 +0700

    Add book controller and view was created.

---
+++ src/main/java/com/github/nibavs/bookcatalog/controller/AddBookController.java
@@ -0,0 +1,96 @@
+package com.github.nibavs.bookcatalog.controller;
+
+import com.github.nibavs.bookcatalog.model.Book;
+import com.github.nibavs.bookcatalog.model.Status;
+import javafx.beans.value.ChangeListener;
+import javafx.beans.value.ObservableValue;
+import javafx.fxml.FXML;
+import javafx.scene.control.ChoiceBox;
+import javafx.scene.control.Label;
+import javafx.scene.control.TextField;
+import javafx.stage.Stage;
+
+public class AddBookController {
+    private Stage dialogStage;
+    private Book newBook;
+
+    @FXML
+    private Label modalTitleLabel;
+
+    @FXML
+    private TextField bookTitleField;
+
+    @FXML
+    private TextField bookAuthorField;
+
+    @FXML
+    private TextField bookYearField;
+
+    @FXML
+    private TextField bookPagesField;
+
+    @FXML
+    private Label bookStatusLabel;
+
+    @FXML
+    private ChoiceBox<Status> bookStatusChoiceBox;
+
+
+    @FXML
+    protected void initialize() {
+        // Input only integer handling
+        // For year
+        bookYearField.textProperty().addListener(new ChangeListener<String>() {
+            @Override
+            public void changed(ObservableValue<? extends String> observable, String oldValue,
+                                String newValue) {
+                if (!newValue.matches("\\d*")) {
+                    bookYearField.setText(newValue.replaceAll("[^\\d]", ""));
+                }
+            }
+        });
+        // For pages
+        bookPagesField.textProperty().addListener(new ChangeListener<String>() {
+            @Override
+            public void changed(ObservableValue<? extends String> observable, String oldValue,
+                                String newValue) {
+                if (!newValue.matches("\\d*")) {
+                    bookPagesField.setText(newValue.replaceAll("[^\\d]", ""));
+                }
+            }
+        });
+
+        bookStatusChoiceBox.getItems().addAll(Status.values());
+    }
+
+    @FXML
+    protected void onConfirmButtonClicked() {
+        // Error handling here
+        String bookTitle = bookTitleField.getText();
+        String bookAuthor = bookAuthorField.getText();
+        int bookYear = Integer.parseInt(bookYearField.getText());
+        int bookPages = Integer.parseInt(bookPagesField.getText());
+        Status bookStatus = bookStatusChoiceBox.getValue();
+
+        newBook = new Book(bookTitle, bookAuthor, bookYear, bookPages, bookStatus);
+
+        dialogStage.close();
+    }
+
+    @FXML
+    protected void onCancelButtonClicked() {
+        dialogStage.close();
+    }
+
+    public void setDialogStage(Stage dialogStage) {
+        this.dialogStage = dialogStage;
+    }
+
+    public Book getNewBook() {
+        return newBook;
+    }
+
+
+
+
+}
