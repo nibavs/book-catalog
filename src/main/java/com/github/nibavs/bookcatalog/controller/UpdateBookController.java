c252130b8bafe064d477db5e88b85b9e1f471076
Nikita Bavs <nbavsunovsky@gmail.com>
2025-01-19 20:31:42 +0700

    UpdateBook Controller and view are created.

---
+++ src/main/java/com/github/nibavs/bookcatalog/controller/UpdateBookController.java
@@ -0,0 +1,106 @@
+package com.github.nibavs.bookcatalog.controller;
+
+import com.github.nibavs.bookcatalog.model.Book;
+import com.github.nibavs.bookcatalog.model.Status;
+import javafx.fxml.FXML;
+import javafx.scene.control.ChoiceBox;
+import javafx.scene.control.Label;
+import javafx.scene.control.TextField;
+import javafx.scene.text.Font;
+import javafx.stage.Stage;
+
+public class UpdateBookController {
+    private Stage dialogStage;
+    private Book updatedBook;
+    private boolean isEdited;
+
+    @FXML
+    private Label modalTitleLabel;
+
+    @FXML
+    private TextField bookTitleField;
+
+    @FXML
+    private TextField bookAuthorField;
+
+    @FXML
+    private TextField bookYearField;
+
+    @FXML
+    private TextField bookPagesField;
+
+    @FXML
+    private Label bookStatusLabel;
+
+    @FXML
+    private ChoiceBox<Status> bookStatusChoiceBox;
+
+
+    @FXML
+    protected void initialize() {
+        modalTitleLabel.setFont(new Font("Arial", 24));
+        // Input only integer handling
+        // For year
+        bookYearField.textProperty().addListener((observable, oldValue, newValue) -> {
+            if (!newValue.matches("\\d*")) {
+                bookYearField.setText(newValue.replaceAll("[^\\d]", ""));
+            }
+        });
+        // For pages
+        bookPagesField.textProperty().addListener((observable, oldValue, newValue) -> {
+            if (!newValue.matches("\\d*")) {
+                bookPagesField.setText(newValue.replaceAll("[^\\d]", ""));
+            }
+
+        });
+
+        bookStatusChoiceBox.getItems().addAll(Status.values());
+
+    }
+
+    @FXML
+    protected void onConfirmButtonClicked() {
+        // Error handling here
+        String bookTitle = bookTitleField.getText();
+        String bookAuthor = bookAuthorField.getText();
+        int bookYear = Integer.parseInt(bookYearField.getText());
+        int bookPages = Integer.parseInt(bookPagesField.getText());
+        Status bookStatus = bookStatusChoiceBox.getValue();
+
+        updatedBook.setTitle(bookTitle);
+        updatedBook.setAuthor(bookAuthor);
+        updatedBook.setYear(bookYear);
+        updatedBook.setPages(bookPages);
+        updatedBook.setStatus(bookStatus);
+
+        isEdited = true;
+        dialogStage.close();
+    }
+
+    @FXML
+    protected void onCancelButtonClicked() {
+        isEdited = false;
+        dialogStage.close();
+    }
+
+    public void setDialogStage(Stage dialogStage) {
+        this.dialogStage = dialogStage;
+    }
+
+    public void setUpdatedBook(Book updatedBook) {
+        this.updatedBook = updatedBook;
+        bookTitleField.setText(updatedBook.getTitle());
+        bookAuthorField.setText(updatedBook.getAuthor());
+        bookYearField.setText(updatedBook.getYear() + "");
+        bookPagesField.setText(updatedBook.getPages() + "");
+        bookStatusChoiceBox.getSelectionModel().select(updatedBook.getStatus());
+    }
+
+    public Book getUpdatedBook() {
+        return updatedBook;
+    }
+
+    public boolean isEdited() {
+        return isEdited;
+    }
+}
