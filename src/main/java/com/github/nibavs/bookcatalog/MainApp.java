d4814838536e0ab3ced33f2cdac7ffec5e8969b4
Nikita Bavs <nbavsunovsky@gmail.com>
2025-01-19 20:25:06 +0700

    scene size changed

--- src/main/java/com/github/nibavs/bookcatalog/MainApp.java
+++ src/main/java/com/github/nibavs/bookcatalog/MainApp.java
@@ -11,7 +11,7 @@ public class MainApp extends Application {
     @Override
     public void start(Stage stage) throws IOException {
         FXMLLoader fxmlLoader = new FXMLLoader(MainApp.class.getResource("MainView.fxml"));
-        Scene scene = new Scene(fxmlLoader.load(), 800, 600);
+        Scene scene = new Scene(fxmlLoader.load(), 900, 700);
         stage.setTitle("Book Catalog");
         stage.setScene(scene);
         stage.show();
